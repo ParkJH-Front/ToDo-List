22efe264209c4d765de901323767725f7ce40dfd
pocopoco <jae1590@gmail.com>
2022-03-02 21:14:34 +0900

    22.03.02
    집에서 더해보자

--- toddlist-re/js/todo-list.js
+++ toddlist-re/js/todo-list.js
@@ -1,33 +1,58 @@
 const todoForm = document.getElementById("todo-form")
 const todoInput = document.getElementById("todoInput")
-const todoBox = document.getElementById("todo-box")
+const todoList = document.getElementById("todo-list")
 
 const TODONAME = "TODONAME"
 let todoArray = []
 
+function saveArray() {
+    localStorage.setItem(TODONAME, JSON.stringify(todoArray))
+}
 // todo-box 내 todoInput 값을 넣은 li 요소 생성
-function printTodo(event) {
+function todoHandler(event) {
     event.preventDefault()
     const todoText = todoInput.value
-    todoInput = ""
-    todoObj = {
+    todoInput.value = ""
+    const todoObj = {
         text:todoText,
-        id:Math.random(0,1)
+        id:Math.random(0,1),
     }
-
-    localSave(todoText)
-
-    const todoList = document.createElement("li")
-    todoList.innerText = todoText
-    todoBox.appendChild(todoList)
-
-    console.log(todoArray)
+    todoArray.push(todoObj)
+    
+    paintTodo(todoObj)
+    saveArray()
 }
 
-function localSave(text) {
-    let TODOID = Math.random(0.1)
-    const ArrayA = { TODOID : text }
-    localStorage.setItem(TODONAME, todoArray)
+function paintTodo(todoObj) {
+    const todoli = document.createElement("li")
+    todoli.id = todoObj.id
+    const todospan = document.createElement("span")
+    todospan.innerText = todoObj.text
+    const todoBtn = document.createElement("button")
+    todoBtn.innerHTML = "✔"
+    todoBtn.addEventListener("click", delectTodo)
+
+    todoli.appendChild(todospan)
+    todoli.appendChild(todoBtn)
+    
+    todoList.appendChild(todoli)
 }
 
-todoForm.addEventListener("submit", printTodo)
+
+function delectTodo(event) {
+    const li = event.target.parentElement
+    li.remove()
+
+    // todoArray = todoArray.filter((todoArray) => todoArray.id !== parseInt(li.id))
+    todoArray = todoArray.filter((toDo) => toDo.id !== parseInt(li.id))
+    saveArray()
+}
+
+const checkTodoList = localStorage.getItem(TODONAME)
+
+todoForm.addEventListener("submit", todoHandler)
+
+if (checkTodoList !== null) {
+    const updateLocalSave = JSON.parse(checkTodoList)
+    updateLocalSave.forEach((item) => paintTodo(item))
+}
