0460120c6285cff5aac34c6c513f2d601bcc2d2d
pocopoco <jae1590@gmail.com>
2022-03-04 20:56:58 +0900

    2022.03.04
    todo list 생성완료
    타입을 조심하자
    
    css 작업중

--- toddlist-re/js/todo-list.js
+++ toddlist-re/js/todo-list.js
@@ -1,58 +1,63 @@
-const todoForm = document.getElementById("todo-form")
-const todoInput = document.getElementById("todoInput")
-const todoList = document.getElementById("todo-list")
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todo-list");
 
-const TODONAME = "TODONAME"
+const TODONAME = "TODONAME";
 let todoArray = []
 
-function saveArray() {
-    localStorage.setItem(TODONAME, JSON.stringify(todoArray))
+function saveTodo() {
+    localStorage.setItem(TODONAME, JSON.stringify(todoArray));
 }
 // todo-box 내 todoInput 값을 넣은 li 요소 생성
 function todoHandler(event) {
-    event.preventDefault()
-    const todoText = todoInput.value
-    todoInput.value = ""
+    event.preventDefault();
+    const todoText = todoInput.value;
+    todoInput.value = "";
     const todoObj = {
         text:todoText,
         id:Math.random(0,1),
     }
-    todoArray.push(todoObj)
+    todoArray.push(todoObj);
     
-    paintTodo(todoObj)
-    saveArray()
-}
+    paintTodo(todoObj);
+    saveTodo();
+}   
 
 function paintTodo(todoObj) {
-    const todoli = document.createElement("li")
-    todoli.id = todoObj.id
-    const todospan = document.createElement("span")
-    todospan.innerText = todoObj.text
-    const todoBtn = document.createElement("button")
-    todoBtn.innerHTML = "✔"
-    todoBtn.addEventListener("click", delectTodo)
-
-    todoli.appendChild(todospan)
-    todoli.appendChild(todoBtn)
+    const todoli = document.createElement("li");
+    todoli.className = "list-group-item border-0 d-flex align-items-center ps-0"
+    todoli.id = todoObj.id;
     
-    todoList.appendChild(todoli)
+    const todospan = document.createElement("span");
+    todospan.innerText = `${todoObj.text}  `;
+    
+    const todoBtn = document.createElement("button");
+    todoBtn.innerHTML = "delect";
+    todoBtn.className = "btn btn-danger btn-sm"
+    todoBtn.addEventListener("click", deleteTodo);
+
+    todoli.appendChild(todospan);
+    todoli.appendChild(todoBtn);
+    
+    todoList.appendChild(todoli);
 }
 
 
-function delectTodo(event) {
-    const li = event.target.parentElement
-    li.remove()
-
-    // todoArray = todoArray.filter((todoArray) => todoArray.id !== parseInt(li.id))
-    todoArray = todoArray.filter((toDo) => toDo.id !== parseInt(li.id))
-    saveArray()
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+    // console.log(typeof li.id)
+    // console.log(typeof todoArray.pop().id)
+    todoArray = todoArray.filter((todo) => todo.id !== String(li.id));
+    saveTodo();
 }
 
-const checkTodoList = localStorage.getItem(TODONAME)
+const checkTodoList = localStorage.getItem(TODONAME);
 
-todoForm.addEventListener("submit", todoHandler)
+todoForm.addEventListener("submit", todoHandler);
 
 if (checkTodoList !== null) {
-    const updateLocalSave = JSON.parse(checkTodoList)
-    updateLocalSave.forEach((item) => paintTodo(item))
+    const updateLocalSave = JSON.parse(checkTodoList);
+    todoArray = updateLocalSave;
+    updateLocalSave.forEach(paintTodo);
 }
